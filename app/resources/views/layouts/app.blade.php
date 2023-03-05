c356b19629d405c82e2e3f19637c0af9a9a0519c
Michael Kachi <jesmikky@gmail.com>
2023-03-05 13:17:57 +0100

    added folder

--- app/resources/views/layouts/app.blade.php
+++ app/resources/views/layouts/app.blade.php
@@ -14,19 +14,19 @@
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,600,700" rel="stylesheet"/>
 
     <!--== All Vendor CSS ==-->
-    <link href="{{asset('assets/css/slicknav.min.css')}}" rel="stylesheet"/>
-    <link href="{{asset('assets/css/slick.min.css')}}" rel="stylesheet"/>
-    <link href="{{asset('assets/css/settings.css')}}" rel="stylesheet"/>
-    <link href="{{asset('assets/css/pe-7-stroke-icon.css')}}" rel="stylesheet"/>
-    <link href="{{asset('assets/css/nice-select.min.css')}}" rel="stylesheet"/>
-    <link href="{{asset('assets/css/magnific-popup.min.css')}}" rel="stylesheet"/>
-    <link href="{{asset('assets/css/leaflet.min.css')}}" rel="stylesheet"/>
-    <link href="{{asset('assets/css/jquery-ui.min.css')}}" rel="stylesheet"/>
-    <link href="{{asset('assets/css/helper.min.css')}}" rel="stylesheet"/>
-    <link href="{{asset('assets/css/font-awesome.min.css')}}" rel="stylesheet"/>
-    <link href="{{asset('assets/css/bootstrap.min.css')}}" rel="stylesheet"/>
+    <link href="{{asset('/assets/css/slicknav.min.css')}}" rel="stylesheet"/>
+    <link href="{{asset('/assets/css/slick.min.css')}}" rel="stylesheet"/>
+    <link href="{{asset('/assets/css/settings.css')}}" rel="stylesheet"/>
+    <link href="{{asset('/assets/css/pe-7-stroke-icon.css')}}" rel="stylesheet"/>
+    <link href="{{asset('/assets/css/nice-select.min.css')}}" rel="stylesheet"/>
+    <link href="{{asset('/assets/css/magnific-popup.min.css')}}" rel="stylesheet"/>
+    <link href="{{asset('/assets/css/leaflet.min.css')}}" rel="stylesheet"/>
+    <link href="{{asset('/assets/css/jquery-ui.min.css')}}" rel="stylesheet"/>
+    <link href="{{asset('/assets/css/helper.min.css')}}" rel="stylesheet"/>
+    <link href="{{asset('/assets/css/font-awesome.min.css')}}" rel="stylesheet"/>
+    <link href="{{asset('/assets/css/bootstrap.min.css')}}" rel="stylesheet"/>
     <!--== Main Style CSS ==-->
-    <link href="{{asset('assets/css/style.css')}}" rel="stylesheet"/>
+    <link href="{{asset('/assets/css/style.css')}}" rel="stylesheet"/>
 
     <!--[if lt IE 9]>
     <script src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
