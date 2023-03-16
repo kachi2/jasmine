2229320500357a55ea409e9e24fd63981027dbd5
Michael Kachi <jesmikky@gmail.com>
2023-03-16 13:57:20 +0100

    updatdz

--- app/resources/views/frontend/minimal/slider.blade.php
+++ app/resources/views/frontend/minimal/slider.blade.php
@@ -29,7 +29,7 @@
                      </div>
  
                      <!-- LAYER NR. 3 -->
-                     <h1 style="color:#fff" class="tp-caption   tp-resizeme" id="slide-1-layer-3" data-x="['{{$slider->align}}','{{$slider->align}}','{{$slider->align}}','{{$slider->align}}']"
+                     <h1 class="tp-caption   tp-resizeme" id="slide-1-layer-3" data-x="['{{$slider->align}}','{{$slider->align}}','{{$slider->align}}','{{$slider->align}}']"
                          data-hoffset="['10','50','10','15']" data-y="['middle','middle','middle','middle']"
                          data-voffset="['0','0','-30','-30']" data-fontsize="['70','70','70','40']"
                          data-lineheight="['70','70','70','40']" data-width="none" data-height="none"
@@ -37,7 +37,7 @@
                          data-frames='[{"delay":50,"speed":1000,"frame":"0","from":"x:-50px;opacity:0;","to":"o:1;","ease":"Power2.easeOut"},{"delay":"wait","speed":300,"frame":"999","to":"opacity:0;","ease":"Power2.easeIn"}]'
                          data-textAlign="['left','left','left','left']" data-paddingtop="[0,0,0,0]"
                          data-paddingright="[0,0,0,0]" data-paddingbottom="[0,0,0,0]" data-paddingleft="[0,0,0,0]">
-                         {!! $slider->title !!}</h1>
+                         <span style="color:#fff" > {!! $slider->title !!}</span></h1>
  
                      <!-- LAYER NR. 4 -->
                      <div class="tp-caption   tp-resizeme" id="slide-1-layer-4" data-x="['{{$slider->align}}','{{$slider->align}}','{{$slider->align}}','{{$slider->align}}']"
