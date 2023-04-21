fb05d861644dd2223dc74393e13a644ba693d818
Michael Kachi <jesmikky@gmail.com>
2023-04-21 16:50:42 +0100

    updats

--- app/resources/views/frontend/minimal/slider.blade.php
+++ app/resources/views/frontend/minimal/slider.blade.php
@@ -32,8 +32,8 @@
                      <h1 class="tp-caption   tp-resizeme" id="slide-1-layer-3" data-x="['{{$slider->align}}','{{$slider->align}}','{{$slider->align}}','{{$slider->align}}']"
                          data-hoffset="['10','50','10','15']" data-y="['middle','middle','middle','middle']"
                          data-voffset="['0','0','-30','-30']" data-fontsize="['70','70','70','40']"
-                         data-lineheight="['70','70','70','40']" data-width="none" data-height="none"
-                         data-whitespace="nowrap" data-type="text" data-responsive_offset="on"
+                         data-lineheight="['70','70','70','40']" data-width="80%" data-height="none"
+                         data-whitespace="rap" data-type="text" data-responsive_offset="on"
                          data-frames='[{"delay":50,"speed":1000,"frame":"0","from":"x:-50px;opacity:0;","to":"o:1;","ease":"Power2.easeOut"},{"delay":"wait","speed":300,"frame":"999","to":"opacity:0;","ease":"Power2.easeIn"}]'
                          data-textAlign="['left','left','left','left']" data-paddingtop="[0,0,0,0]"
                          data-paddingright="[0,0,0,0]" data-paddingbottom="[0,0,0,0]" data-paddingleft="[0,0,0,0]">
@@ -43,7 +43,7 @@
                      <div class="tp-caption   tp-resizeme" id="slide-1-layer-4" data-x="['{{$slider->align}}','{{$slider->align}}','{{$slider->align}}','{{$slider->align}}']"
                           data-hoffset="['10','50','10','15']" data-y="['middle','middle','middle','middle']"
                           data-voffset="['61','61','31','14']" data-width="none" data-height="none"
-                          data-whitespace="nowrap" data-type="text" data-responsive_offset="on"
+                          data-whitespace="rap" data-type="text" data-responsive_offset="on"
                           data-frames='[{"delay":500,"speed":2000,"frame":"0","from":"y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0;","mask":"x:0px;y:[100%];s:inherit;e:inherit;","to":"o:1;","ease":"Power2.easeInOut"},{"delay":"wait","speed":300,"frame":"999","to":"opacity:0;","ease":"nothing"}]'
                           data-textAlign="['left','left','left','left']" data-paddingtop="[0,0,0,0]"
                           data-paddingright="[0,0,0,0]" data-paddingbottom="[0,0,0,0]" data-paddingleft="[0,0,0,0]">
