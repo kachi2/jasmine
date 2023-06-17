4db0830a560519b1519a8b3bcc2319dcf4fb4a49
Michael Kachi <jesmikky@gmail.com>
2023-06-17 10:38:09 +0100

    updates

--- app/resources/views/frontend/minimal/slider.blade.php
+++ app/resources/views/frontend/minimal/slider.blade.php
@@ -43,7 +43,7 @@
                      <div class="tp-caption   tp-resizeme" id="slide-1-layer-4" data-x="['{{$slider->align}}','{{$slider->align}}','{{$slider->align}}','{{$slider->align}}']"
                           data-hoffset="['10','50','10','15']" data-y="['middle','middle','middle','middle']"
                           data-voffset="['61','61','31','14']" data-width="100%" data-height="none"
-                          data-whitespace="rap" data-type="text" data-responsive_offset="on"
+                          data-whitespace="wrap" data-type="text" data-responsive_offset="on"
                           data-frames='[{"delay":500,"speed":2000,"frame":"0","from":"y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0;","mask":"x:0px;y:[100%];s:inherit;e:inherit;","to":"o:1;","ease":"Power2.easeInOut"},{"delay":"wait","speed":300,"frame":"999","to":"opacity:0;","ease":"nothing"}]'
                           data-textAlign="['{{$slider->align}}','{{$slider->align}}','{{$slider->align}}','{{$slider->align}}']" data-paddingtop="[0,0,0,0]"
                           data-paddingright="[0,0,0,0]" data-paddingbottom="[0,0,0,0]" data-paddingleft="[0,0,0,0]">
